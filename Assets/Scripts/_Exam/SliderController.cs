e6630b2bdbf3cfc4d54abece2a9afca67eb1ce43
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-11-17 19:42:07 +0900

    [add]タイムバーとテキスト表示のテストコード追加

---
+++ Assets/Scripts/_Exam/SliderController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    private float value;
+    private float maxValue = 100f;
+
+    public float moveSpeed = 50f;
+
+    private Slider slider;
+
+    public GameObject textObj;
+    public TextManager textManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = /* this.gameObject. */GetComponent<Slider>();
+        slider.maxValue = maxValue;
+        value = maxValue;
+
+        textManager = textObj.GetComponent<TextManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // isEnd == false
+        // if(!textManager.isEnd)
+        // {
+            value -= moveSpeed * Time.deltaTime;
+
+            // スライダーが0になったとき
+            if(value < 0)
+            {
+                textManager.SetTexts();
+                ResetSliderValue();    
+            };           
+        // }
+        // else
+        // {
+        //     ResetSliderValue();
+        //     Debug.Log("おわり");
+        // }
+
+        slider.value = value;
+    }
+
+    // スライダーの量を最大値にリセットする
+    public void ResetSliderValue()
+    {
+        value = maxValue;    
+    }
+}
---
+++ Assets/Scripts/_Exam/TextManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TextManager : MonoBehaviour
+{
+    public List<string> texts = new List<string>();
+    Text text;
+
+    private int index = 0;
+
+    public bool isEnd = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        text.text = texts[index];
+    }
+
+    public void SetTexts()
+    {
+        try
+        {
+            index++;
+            text.text = texts[index];
+        }catch(ArgumentOutOfRangeException e)
+        {
+            text.text = "おわり";
+            isEnd = true;
+            //　時を止める
+            Time.timeScale = 0f;
+        }
+    }
+}
